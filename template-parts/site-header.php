07bfb720110202ca10843a1babd775735d29f217
Marcin Pietrzak <marcin@iworks.pl>
2021-04-05 15:40:56 +0200

    better header

--- template-parts/site-header.php
+++ template-parts/site-header.php
@@ -2,53 +2,58 @@
 
 $args = wp_parse_args(
 	$args,
-	[
+	array(
 		'buttons' => 'hide',
-	]
+	)
 );
 
 ?><header id="masthead" class="site-header">
 	<div class="site-header-one-container">
 		<div class="site-header-one">
-			<div class="site-branding">
 <?php
-the_custom_logo();
-if ( is_front_page() && is_home() ) {
-	?>
-<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-	<?php
-} else {
-	?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-	<?php
+$tag = 'p';
+if ( is_front_page() ) {
+	$tag = 'h1';
 }
 ?>
-<p class="site-description">
-    <strong><?php esc_html_e( 'sailors, boats & results', '5o5-results-archive' ); ?></strong>
-    <span><?php esc_html_e( 'through the ages', '5o5-results-archive' ); ?></span>
-</p>
-            </div><!-- .site-branding -->
-            <div class="search"><?php get_search_form(); ?></div>
+<<?php echo $tag; ?> class="site-branding">
+	<?php
+	if ( ! is_front_page() ) {
+		?>
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php } ?>
+<span class="site-title"><?php bloginfo( 'name' ); ?></span>
+<span class="site-description">
+	<span>
+		<strong><?php esc_html_e( 'sailors, boats & results', '5o5-results-archive' ); ?></strong>
+		<span><?php esc_html_e( 'through the ages', '5o5-results-archive' ); ?></span>
+	</span>
+</span>
+	<?php
+	if ( ! is_front_page() ) {
+		?>
+		</a><?php } ?>
+</<?php echo $tag; ?>>
+			<div class="search"><?php get_search_form(); ?></div>
 			<nav id="site-navigation">
 				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', '5o5-results-archive' ); ?></button>
-                <div id="primary-menu">
+				<div id="primary-menu">
 <?php if ( has_nav_menu( 'primary' ) ) { ?>
-                    <ul class="primary-menu reset-list-style">
-<?php
+					<ul class="primary-menu reset-list-style">
+	<?php
 									wp_nav_menu(
 										array(
 											'container'  => '',
 											'items_wrap' => '%3$s',
-                                            'theme_location' => 'primary',
-                                            'depth' => 1,
+											'theme_location' => 'primary',
+											'depth'      => 1,
 										)
-                                    );
-                                }
+									);
+}
 ?>
-                    </ul>
+					</ul>
 				</div>
 			</nav>
-        </div>
-    </div>
+		</div>
+	</div>
 </header>
 
