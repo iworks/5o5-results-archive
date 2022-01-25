2e09a8a1635fbd5ffff2ad89e7965864901d0f15
Marcin Pietrzak <marcin@iworks.pl>
2022-01-25 11:15:42 +0100

    remove pwa

--- inc/class-int5o5-archive-theme.php
+++ inc/class-int5o5-archive-theme.php
@@ -23,24 +23,17 @@ class Int5o5_Archive_Theme extends Int5o5_Archive {
 		 */
 		add_action( 'wp_head', array( $this, 'html_head' ), PHP_INT_MAX );
 		add_filter( 'get_site_icon_url', array( $this, 'get_site_default_icon_url' ), 10, 3 );
-		add_filter( 'site_icon_meta_tags', array( $this, 'site_icon_meta_tags' ) );
-		add_action( 'parse_request', array( $this, 'browserconfig_xml' ) );
 		add_action( 'parse_request', array( $this, 'request_favicon' ) );
 		add_action( 'init', array( $this, 'register_scripts' ) );
 		add_action( 'save_post', array( $this, 'cache_clear' ) );
 		add_filter( 'document_title_parts', array( $this, 'document_title_parts' ) );
 		add_action( 'after_setup_theme', array( $this, 'register_nav_menus' ) );
 		/**
-		 * manifest.json
+		 * PWA
 		 */
-		if ( class_exists( 'iWorks_PWA' ) ) {
-			add_filter( 'iworks_pwa_configuration', array( $this, 'iworks_pwa_configuration' ) );
-			add_filter( 'iworks_pwa_offline_svg', array( $this, 'iworks_pwa_offline_svg' ) );
-			add_filter( 'iworks_pwa_offline_file', array( $this, 'iworks_pwa_offline_file' ) );
-			add_filter( 'iworks_pwa_offline_urls_set', array( $this, 'iworks_pwa_offline_urls_set' ) );
-		} else {
-			add_action( 'parse_request', array( $this, 'manifest_json' ) );
-		}
+		add_filter( 'iworks_pwa_offline_svg', array( $this, 'iworks_pwa_offline_svg' ) );
+		add_filter( 'iworks_pwa_offline_file', array( $this, 'iworks_pwa_offline_file' ) );
+		add_filter( 'iworks_pwa_offline_urls_set', array( $this, 'iworks_pwa_offline_urls_set' ) );
 		/**
 		 * speed
 		 */
@@ -80,10 +73,6 @@ class Int5o5_Archive_Theme extends Int5o5_Archive {
 
 	public function iworks_pwa_offline_file( $data ) {
 		return file_get_contents( $this->root . '/assets/pwa/offline.html' );
-	}
-
-	public function iworks_pwa_configuration( $data ) {
-		return wp_parse_args( $this->manifest_json_data(), $data );
 	}
 
 	public function iworks_pwa_offline_svg( $svg ) {
@@ -155,7 +144,6 @@ class Int5o5_Archive_Theme extends Int5o5_Archive {
 		 * turn off iOS phone number scraping
 		 */
 		echo '<meta name="format-detection" content="telephone=no" />' . PHP_EOL;
-		echo '<meta name="msapplication-config" content="/browserconfig.xml" />' . PHP_EOL;
 		if ( is_singular() && pings_open() ) {
 			printf( '<link rel="pingback" href="%s">', esc_url( get_bloginfo( 'pingback_url' ) ) );
 		}
@@ -174,80 +162,6 @@ class Int5o5_Archive_Theme extends Int5o5_Archive {
 	}
 
 	/**
-	 * Favicons + meta settings
-	 *
-	 * @since 1.0.0
-	 */
-	public function site_icon_meta_tags( $meta_tags ) {
-		$meta_tags = array();
-		$icons     = array(
-			'icon'    => array(
-				16,
-				32,
-				96,
-			),
-			'apple'   => array(
-				57,
-				60,
-				72,
-				76,
-				114,
-				120,
-				152,
-				180,
-			),
-			'android' => array(
-				192,
-				512,
-			),
-		);
-		foreach ( $icons as $type => $sizes ) {
-			foreach ( $sizes as $size ) {
-				$s    = sprintf( '%1$dx%1$d', $size );
-				$mask = $file = '';
-				switch ( $type ) {
-					case 'icon':
-						$file = sprintf( 'favicon-%s', $s );
-						$mask = '<link rel="icon" type="image/png" sizes="%1$s" href="%2$s" />';
-						break;
-					case 'apple':
-						$file = sprintf( 'apple-icon-%s', $s );
-						$mask = '<link rel="apple-touch-icon" sizes="%1$s" href="%2$s" />';
-						break;
-					case 'android':
-						$file = sprintf( 'android-icon-%s', $s );
-						$mask = '<link rel="apple-touch-icon" sizes="%1$s" href="%2$s" />';
-						break;
-				}
-				if ( ! empty( $mask ) && ! empty( $file ) ) {
-					$meta_tags[] = sprintf( $mask, $s, $this->get_favicon_url( $file ) );
-				}
-			}
-		}
-		$meta_tags[] = sprintf(
-			'<link rel="shortcut icon" href="%s" />',
-			$this->get_favicon_url( 'favicon', 'ico' )
-		);
-		$meta_tags[] = sprintf(
-			'<link rel="mask-icon" href="%s" color="#5bbad5" />',
-			$this->get_favicon_url( 'safari-pinned-tab', 'svg' )
-		);
-		$meta_tags[] = sprintf( '<meta name="msapplication-TileColor" content="%s">', esc_attr( $this->color_title ) );
-		$meta_tags[] = sprintf( '<meta name="theme-color" content="%s" />', esc_attr( $this->color_theme ) );
-		$meta_tags[] = sprintf(
-			'<meta name="msapplication-TileImage" content="%s" />',
-			$this->get_favicon_url( 'ms-icon-144x144' )
-		);
-		/**
-		 * manifest.json
-		 */
-		if ( ! class_exists( 'iWorks_PWA' ) ) {
-			$meta_tags[] = '<link rel="manifest" href="/manifest.json" />';
-		}
-		return $meta_tags;
-	}
-
-	/**
 	 * Handle "/favicon.json" request.
 	 *
 	 * @since 1.0.0
@@ -261,122 +175,6 @@ class Int5o5_Archive_Theme extends Int5o5_Archive {
 			return;
 		}
 		header( 'Location: ' . $this->get_favicon_url( 'favicon', 'ico' ) );
-		exit;
-	}
-
-	/**
-	 * Handle "/browserconfig.xml" request.
-	 *
-	 * @since 1.0.0
-	 */
-	public function browserconfig_xml() {
-		if (
-			! isset( $_SERVER['REQUEST_URI'] ) ) {
-			return;
-		}
-		if ( '/browserconfig.xml' !== $_SERVER['REQUEST_URI'] ) {
-			return;
-		}
-		header( 'Content-type: text/xml' );
-		echo '<' . '?xml version="1.0" encoding="utf-8"?' . '>';
-		echo PHP_EOL;
-		echo '<browserconfig>';
-		echo '<msapplication>';
-		echo '<tile>';
-		$sizes = array( 70, 150, 310 );
-		foreach ( $sizes as $size ) {
-			$url = $this->get_asset_url(
-				sprintf(
-					'icons/favicon/ms-icon-%1$dx%1$d.png',
-					$size
-				)
-			);
-			printf( '<square%1$dx%1$dlogo src="%2$s"/>', $size, esc_url( $url ) );
-		}
-		printf( '<TileColor>%s</TileColor>', $this->color_title );
-		echo '</tile>';
-		echo '</msapplication>';
-		echo '</browserconfig>';
-		exit;
-	}
-
-	private function manifest_json_data() {
-		return array(
-			'name'             => get_bloginfo( 'sitename' ),
-			'short_name'       => $this->short_name,
-			'theme_color'      => $this->color_theme,
-			'background_color' => $this->color_background,
-			'display'          => 'standalone',
-			'Scope'            => '/',
-			'start_url'        => '/',
-			'icons'            => array(
-				array(
-					'src'     => esc_url( $this->get_asset_url( 'icons/favicon/android-icon-36x36.png' ) ),
-					'sizes'   => '36x36',
-					'type'    => 'image/png',
-					'density' => '0.75',
-				),
-				array(
-					'src'     => esc_url( $this->get_asset_url( 'icons/favicon/android-icon-48x48.png' ) ),
-					'sizes'   => '48x48',
-					'type'    => 'image/png',
-					'density' => '1.0',
-				),
-				array(
-					'src'     => esc_url( $this->get_asset_url( 'icons/favicon/android-icon-72x72.png' ) ),
-					'sizes'   => '72x72',
-					'type'    => 'image/png',
-					'density' => '1.5',
-				),
-				array(
-					'src'     => esc_url( $this->get_asset_url( 'icons/favicon/android-icon-96x96.png' ) ),
-					'sizes'   => '96x96',
-					'type'    => 'image/png',
-					'density' => '2.0',
-				),
-				array(
-					'src'     => esc_url( $this->get_asset_url( 'icons/favicon/android-icon-144x144.png' ) ),
-					'sizes'   => '144x144',
-					'type'    => 'image/png',
-					'density' => '3.0',
-				),
-				array(
-					'src'     => esc_url( $this->get_asset_url( 'icons/favicon/android-icon-192x192.png' ) ),
-					'sizes'   => '192x192',
-					'type'    => 'image/png',
-					'density' => '4.0',
-				),
-				array(
-					'src'   => esc_url( $this->get_asset_url( 'icons/favicon/android-icon-512x512.png' ) ),
-					'sizes' => '512x512',
-					'type'  => 'image/png',
-				),
-				array(
-					'src'     => esc_url( $this->get_asset_url( 'icons/favicon/maskable.png' ) ),
-					'sizes'   => '1024x1024',
-					'type'    => 'image/png',
-					'purpose' => 'any maskable',
-				),
-			),
-			'splash_pages'     => null,
-		);
-	}
-
-	/**
-	 * Handle "/manifest.json" request.
-	 *
-	 * @since 1.0.0
-	 */
-	public function manifest_json() {
-		if (
-			! isset( $_SERVER['REQUEST_URI'] ) ) {
-			return;
-		}
-		if ( '/manifest.json' !== $_SERVER['REQUEST_URI'] ) {
-			return;
-		}
-		header( 'Content-Type: application/json' );
-		echo json_encode( $this->manifest_json_data() );
 		exit;
 	}
 
